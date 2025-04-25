aafbc0b5b3edf2afabb3753f6b111c8c10a5ae3e
Kenneth Cheung <kennefcc1119@protonmail.com>
2025-04-25 18:07:23 +0800

    update layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,25 +4,32 @@ import streamlit as st
 from datetime import datetime
 import time
 
-st.markdown("<h1 style='font-size:36px;'>❤️ Kandz 英國法國巴黎歐洲名牌代購❤️</h1>", unsafe_allow_html=True)
+st.image("graphics/banner.jpg", use_container_width=True)
+
+st.markdown("""
+<div style='text-align: center;'>
+    <h1 style='font-size:36px;'>❤️ Kandz代購 Secret Promotion Site❤️</h1>
+    <h4 style='font-size:24px;'>For exclusive Hong Kong clients only!!!</h4>
+</div>
+""", unsafe_allow_html=True)
+
 st.markdown("""
 Kandz 已經踏入第9年，一直為大家努力搜羅全球不同嘅名牌手袋、首飾及服裝，致力將最優質嘅時尚單品帶到您嘅身邊。我們專注於代購服務，確保每一件商品都經過嚴格把關，讓您足不出戶即可擁有心儀嘅國際精品。
 
 憑藉多年嘅經驗與專業團隊，我們承諾貨品一般兩星期內到貨，讓您享受快速又安心嘅購物體驗。無論係經典款手袋、限量版首飾，還是潮流服飾，Kandz 都致力為您提供最優惠嘅價格與最貼心嘅服務。選擇 Kandz，讓您嘅時尚之旅更加輕鬆、便捷，盡顯獨特品味與魅力。
 
-📘FB: https://www.facebook.com/kandzzzzz
-
-🟢WT: https://wa.me/447418333672
-
 """)
-
-###################################################################################################
 df = pd.read_excel("excel/products.xlsx")
-cols = st.columns(3)
 now = datetime.now()
 base_image_url = "https://kandz.streamlit.app/images/"
+
 ###################################################################################################
 
+valid_offers = df[df['expiry_date'].apply(lambda x: datetime.strptime(str(x), "%Y%m%d")) > now]
+st.markdown(f"### 🎁 今日 Secret Promotion Offer 數量：<span style='color:red; font-weight:bold;'>{len(valid_offers)}</span>", unsafe_allow_html=True)
+cols = st.columns(3)
+
+###################################################################################################
 for idx, row in df.iterrows():
     expiry_date = datetime.strptime(str(row['expiry_date']), "%Y%m%d")
     expiry_datetime = datetime.combine(expiry_date.date(), datetime.min.time())
@@ -35,6 +42,7 @@ for idx, row in df.iterrows():
         seconds = total_seconds % 60
         col = cols[idx % 3]
         with col:
+            
             image_filename = str(row['image_filename'])
             image_path = Path("images") / (image_filename + ".jpeg")
             pay_url = row['payment_url']
@@ -43,7 +51,7 @@ for idx, row in df.iterrows():
             st.image(image_path, use_container_width=True)
             st.markdown(f"**{row['name']}**")
             st.caption(row['description'])
-            st.markdown(f"⏳ 剩餘時間：{days} 日 {hours} 小時 {minutes} 分鐘 {seconds} 秒")
+            st.markdown(f"⏳ Promotion expires in<br><b>{days} D {hours} Hr {minutes} min {seconds} sec</b>", unsafe_allow_html=True)
 
             message = f"你好，我想查詢 {row['name']} 幾錢?"
             whatsapp_url = f"https://wa.me/447418333672?text={message}"
